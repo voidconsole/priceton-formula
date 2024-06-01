0bd0126f9c3c8928f00333315c08b936f5b4d83e
Satwik Bhusanur <152626693+tesseract-codes@users.noreply.github.com>
2024-06-01 15:58:12 +0530

    Fixed validation bugs

--- app.js
+++ app.js
@@ -32,4 +32,8 @@ function remove() {
 function slide(){
     dom.querySelector("#sl").innerHTML = dom.querySelector('#fac').value
 }
+function slideMob(){
+    dom.querySelector("#slMob").innerHTML = dom.querySelector('#facMob').value
+}
+
 
